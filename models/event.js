bcccf5cff273004ab3c01157f6a611dec11ffcad
brandolern <46857695+brandolern@users.noreply.github.com>
2019-07-09 19:50:12 -0600

    changes to models

--- models/event.js
+++ models/event.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 const eventSchema = new Schema({
 	title: { type: String, required: true, trim: true },
 	date: { type: Date, required: true },
+	details: { type: String, trim: true },
 	location: { type: String, required: true },
 	requirements: [{ type: String, trim: true }],
 	tasks: [
