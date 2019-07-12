14da268a1af36406ddccf03fb81307181c602cef
Scars and Bullets <sbrough@gmail.com>
2019-07-11 19:00:25 -0600

    added react router

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,12 +1,29 @@
 import React from "react";
+import { Col, Row, Container } from "../Grid";
+import { Link } from "react-router-dom";
 
 function Nav() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <a className="navbar-brand" href="/">
-        React Reading List
+    <Container>
+      <nav className="navbar navbar-dark bg-dark">
+        <a className="navbar-brand" href="/">
+          Event Planner
       </a>
-    </nav>
+        <ul>
+          <Link to="/login">
+            <span Link>Sign In </span>
+          </Link>
+          <span li> | </span>
+          <Link to="/login">
+            <span Link>Sign Out</span>
+          </Link>
+          <span li> | </span>
+          <Link to="/about">
+            <span Link>About</span>
+          </Link>
+        </ul>
+      </nav>
+    </Container>
   );
 }
 
