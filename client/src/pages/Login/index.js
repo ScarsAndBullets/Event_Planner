99593ab58a5923f35ba70662e7731d73bf0af936
brandolern <46857695+brandolern@users.noreply.github.com>
2019-07-14 19:13:21 -0600

    signup form posting to database

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -1,28 +1,55 @@
 import React, { Component } from "react";
-import { HashRouter as Route, NavLink } from 'react-router-dom';
-import SignInForm from '../../components/SignInForm';
+import { HashRouter as Route, NavLink } from "react-router-dom";
+import SignInForm from "../../components/SignInForm";
 
 class Login extends Component {
-  render() {
-    return (
-      <div className="App">
-      <div className="App__Aside"></div>
-      <div className="App__Form">
-        <div className="PageSwitcher">
-            <NavLink to="/log-in" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign In</NavLink>
-            <NavLink exact to="/sign-up" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign Up</NavLink>
-          </div>
+	render() {
+		return (
+			<div className="App">
+				<div className="App__Aside" />
+				<div className="App__Form">
+					<div className="PageSwitcher">
+						<NavLink
+							to="/log-in"
+							activeClassName="PageSwitcher__Item--Active"
+							className="PageSwitcher__Item"
+						>
+							Log In
+						</NavLink>
+						<NavLink
+							exact
+							to="/sign-up"
+							activeClassName="PageSwitcher__Item--Active"
+							className="PageSwitcher__Item"
+						>
+							Sign Up
+						</NavLink>
+					</div>
 
-          <div className="FormTitle">
-              <NavLink to="/log-in" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Sign In</NavLink> or <NavLink exact to="/" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Sign Up</NavLink>
-          </div>
+					<div className="FormTitle">
+						<NavLink
+							to="/log-in"
+							activeClassName="FormTitle__Link--Active"
+							className="FormTitle__Link"
+						>
+							Log In
+						</NavLink>{" "}
+						or{" "}
+						<NavLink
+							exact
+							to="/sign-up"
+							activeClassName="FormTitle__Link--Active"
+							className="FormTitle__Link"
+						>
+							Sign Up
+						</NavLink>
+					</div>
 
-          <SignInForm />
-      </div>
-
-    </div>
-    );
-  }
+					<SignInForm />
+				</div>
+			</div>
+		);
+	}
 }
 
 export default Login;
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,10 +1,13 @@
-import decode from 'jwt-decode';
-import axios from 'axios';
+import decode from "jwt-decode";
+import axios from "axios";
 
 export default {
 	// logs in user
 	submitLogin: function(login) {
 		return axios.post("/api/user/login", login);
+	},
+	submitSignup: function(signup) {
+		return axios.post("/api/user/signup", signup);
 	},
 	//Get users events to populate event dashboard
 	getEvents: function() {
@@ -21,69 +24,61 @@ export default {
 };
 
 export class AuthService {
+	login = (email, password) => {
+		// Get a token
+		return axios
+			.post("/api/user/login", { email: email, password: password })
+			.then(res => {
+				// set the token once the user logs in
+				// once the  backend is setup we can use this.
+				// this.setToken(res.data.token);
 
-  login = (email, password) => {
-    // Get a token
-    return axios.post('/api/user/login', { email: email, password: password })
-      .then(res => {
-        // set the token once the user logs in
-        // once the  backend is setup we can use this.
-        // this.setToken(res.data.token);
+				//hardcoding token for now
+				this.setToken("faketoken");
 
-        //hardcoding token for now
-        this.setToken('faketoken');
+				// return the rest of the response
+				return res;
+			});
+	};
 
+	getProfile = () => {
+		// return decode(this.getToken());
+		return decode(this.getToken());
+	};
 
+	loggedIn() {
+		// Checks if there is a saved token and it's still valid
+		const token = this.getToken();
+		return !!token && !this.isTokenExpired(token); // handwaiving here
+	}
 
-        // return the rest of the response
-        return res;
-      });
-  };
+	isTokenExpired(token) {
+		try {
+			const decoded = decode(token);
+			if (decoded.exp < Date.now() / 1000) {
+				return true;
+			} else return false;
+		} catch (err) {
+			return false;
+		}
+	}
 
-  getProfile = () => {
-    // return decode(this.getToken());
-    return decode(this.getToken());
-  };
+	setToken(idToken) {
+		// Saves user token to localStorage
+		axios.defaults.headers.common["Authorization"] = `Bearer ${idToken}`;
+		localStorage.setItem("id_token", idToken);
+	}
 
-  loggedIn() {
-    // Checks if there is a saved token and it's still valid
-    const token = this.getToken();
-    return !!token && !this.isTokenExpired(token) // handwaiving here
-  }
+	getToken() {
+		// Retrieves the user token from localStorage
+		return localStorage.getItem("id_token");
+	}
 
-  isTokenExpired(token) {
-    try {
-      const decoded = decode(token);
-      if (decoded.exp < Date.now() / 1000) {
-        return true;
-      }
-      else
-        return false;
-    }
-    catch (err) {
-      return false;
-    }
-  }
-
-  setToken(idToken) {
-    // Saves user token to localStorage
-    axios.defaults.headers.common['Authorization'] = `Bearer ${idToken}`;
-    localStorage.setItem('id_token', idToken);
-  }
-
-  getToken() {
-    // Retrieves the user token from localStorage
-    return localStorage.getItem('id_token');
-  }
-
-  logout() {
-    // Clear user token and profile data from localStorage
-    axios.defaults.headers.common['Authorization'] = null;
-    localStorage.removeItem('id_token');
-    // this will reload the page and reset the state of the application
-    window.location.reload('/');
-  }
-
-
-
+	logout() {
+		// Clear user token and profile data from localStorage
+		axios.defaults.headers.common["Authorization"] = null;
+		localStorage.removeItem("id_token");
+		// this will reload the page and reset the state of the application
+		window.location.reload("/");
+	}
 }
