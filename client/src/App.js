d54fa4ccdde7ab95a70a032ab2afd288e2b797f9
brandonconover90@gmail.com <brandonconover90@gmail.com>
2019-07-18 17:23:17 -0600

    form

--- client/src/App.js
+++ client/src/App.js
@@ -9,8 +9,8 @@ class App extends Component {
   render() {
     return (
       <div className="demo-big-content App">
-      <Layout>
-          <Nav/>
+        <Layout>
+          <Nav />
           <Content>
             <Main />
           </Content>
