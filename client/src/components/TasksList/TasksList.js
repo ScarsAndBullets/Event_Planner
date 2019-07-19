0143158b175f143e8de0da68be3934a0e35c6d89
Manny Barboza <mannyb1077@gmail.com>
2019-07-18 21:08:10 -0600

    Tasksdb

--- client/src/components/TasksList/TasksList.js
+++ client/src/components/TasksList/TasksList.js
@@ -4,7 +4,7 @@ import React, { Component } from "react";
 
 import API from "../../utils/API";
 
-class Detail extends Component {
+class TasksList extends Component {
   state = {
     task: {}
   };
@@ -19,13 +19,13 @@ class Detail extends Component {
   render() {
     return (
       <Container fluid>
-        <Row>
+        {/* <Row>
           <Col size='md-12'>
             <Jumbotron>
               <h1>{this.state.task.taskName}</h1>
             </Jumbotron>
           </Col>
-        </Row>
+        </Row> */}
 
         <Row>
           <Col size='md-2'>
@@ -38,77 +38,3 @@ class Detail extends Component {
 }
 
 export default TasksList;
-
-// import React, { Component } from "react";
-// import NewTaskForm from "../TasksForm/NewTasksForm";
-// import Task from "../Tasks/task";
-// import "./TasksList.css";
-
-// class TasksList extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       tasks: []
-//     };
-//     this.create = this.create.bind(this);
-//     this.remove = this.remove.bind(this);
-//     this.update = this.update.bind(this);
-//     this.toggleCompletion = this.toggleCompletion.bind(this);
-//   }
-//   create(newTask) {
-//     this.setState({
-//       tasks: [...this.state.tasks, newTask]
-//     });
-//   }
-//   remove(id) {
-//     this.setState({
-//       tasks: this.state.tasks.filter(tsk => tsk.id !== id)
-//     });
-//   }
-//   update(id, updatedTask) {
-//     const updatedTasks = this.state.tasks.map(task => {
-//       if (task.id === id) {
-//         return { ...task, task: updatedTask };
-//       }
-//       return task;
-//     });
-//     this.setState({ tasks: updatedTasks });
-//   }
-//   toggleCompletion(id) {
-//     const updatedTasks = this.state.tasks.map(task => {
-//       if (task.id === id) {
-//         return { ...task, completed: !task.completed };
-//       }
-//       return task;
-//     });
-//     this.setState({ tasks: updatedTasks });
-//   }
-//   render() {
-//     const tasks = this.state.tasks.map(task => {
-//       return (
-//         <Task
-//           key={task.id}
-//           id={task.id}
-//           task={task.task}
-//           completed={task.completed}
-//           removeTask={this.remove}
-//           updatedTask={this.update}
-//           toggleTask={this.toggleCompletion}
-//         />
-//       );
-//     });
-//     return (
-//       <div className='TaskList'>
-//         <h1>
-//           Tasks! <span>Choose what you want to contribute</span>
-//         </h1>
-//         <ul>
-//           <NewTaskForm createTask={this.create} />
-//           {tasks}
-//         </ul>
-//       </div>
-//     );
-//   }
-// }
-
-// export default TasksList;
