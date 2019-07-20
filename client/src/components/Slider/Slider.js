3467efeee6cca83de78e53fd904fa83089369071
hachir <ha1991ha@yahoo.com>
2019-07-19 20:04:07 -0600

    update-slider

--- client/src/components/Slider/Slider.js
+++ client/src/components/Slider/Slider.js
@@ -27,4 +27,60 @@ class Slider extends Component {
               <IconButton name="share" />
             </CardMenu>
           </Card>
-        
+          {/* Event  2 */}
+          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover'}} >React Project #2</CardTitle>
+            <CardText>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+            </CardText>
+            <CardActions border>
+              <Button colored>new Event</Button>
+              <Button cod>pass Event</Button>
+            </CardActions>
+            <CardMenu style={{color: '#fff'}}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+          {/* Event  3 */}
+          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover'}} >New Eventt #3</CardTitle>
+            <CardText>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+            </CardText>
+            <CardActions border>
+              <Button colored>New Event</Button>
+              <Button colored>New Event</Button>
+            </CardActions>
+            <CardMenu style={{color: '#fff'}}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+        </div>
+      )
+    } else if(this.state.activeTab === 1) {
+      return (
+        <div><h1>This is New Event </h1></div>
+      )
+    } else if(this.state.activeTab === 2) {
+      return (
+        <div><h1>This is pass Event</h1></div>
+      )
+    }
+  }
+  render() {
+    return(
+      <div>
+        <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
+          <Tab>New Event</Tab>
+          <Tab>pass Event</Tab>
+        </Tabs>
+          <Grid>
+            <Cell col={12}>
+              <div className="content">{this.toggleCategories()}</div>
+            </Cell>
+          </Grid>
+      </div>
+    )
+  }
+}
+export default Slider;
