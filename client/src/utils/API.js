0e4844f8d9609c5a669c80bfc06c38043bd42a51
Scars and Bullets <sbrough@gmail.com>
2019-07-20 15:11:01 -0600

    added NodeMailer, enabled email sending from inviting participants

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,28 +2,32 @@ import decode from "jwt-decode";
 import axios from "axios";
 
 export default {
-  submitSignup: function(signup) {
+  submitSignup: function (signup) {
     return axios.post("/api/user/signup", signup);
   },
   //Get users events to populate event dashboard
-  getEvents: function() {
+  getEvents: function () {
     return axios.get("/api/events/event-dashboard");
   },
 
-  getTasks: function() {
+  getTasks: function () {
     return axios.get("/api/tasks");
   },
 
-  getTask: function(id) {
+  getTask: function (id) {
     return axios.get("/apit/tasks/" + id);
   },
 
-  deleteTask: function(id) {
+  deleteTask: function (id) {
     return axios.delete("/api/tasks" + id);
   },
 
-  saveTask: function(taskData) {
+  saveTask: function (taskData) {
     return axios.post("api/tasks", taskData);
+  },
+
+  sendEmail: function (email, eventId) {
+    return axios.post(`api/events/${eventId}/send-mail`, { email });
   }
 };
 
---
+++ controllers/mailerController.js
@@ -0,0 +1,34 @@
+let nodemailer = require('nodemailer');
+
+module.exports = {
+
+	sendMail: (req, res) => {
+		console.log(`send mail controller`)
+
+		let transporter = nodemailer.createTransport({
+			service: 'gmail',
+			auth: {
+				user: process.env.NMUSER,
+				pass: process.env.NMPASS
+			}
+		});
+
+		console.log(typeof process.env.NMUSER, process.env.NMUSER, typeof process.env.NMPASS, process.env.NMPASS)
+
+		let mailOptions = {
+			from: process.env.NMUSER,
+			to: req.body.email,
+			subject: "You've been invited to an event in pland",
+			text: `Follow this link to view the event: localhost:3000/api/`
+		};
+
+		transporter.sendMail(mailOptions, function (error, info) {
+			if (error) {
+				console.log(error);
+			} else {
+				console.log("Email sent: " + info.response);
+			}
+		});
+
+	}
+};
