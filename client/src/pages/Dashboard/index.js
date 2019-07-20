9596283883150ce217f617ff12fc17a2c4435632
Manny Barboza <mannyb1077@gmail.com>
2019-07-20 11:08:56 -0600

    d

--- client/src/pages/Dashboard/index.js
+++ client/src/pages/Dashboard/index.js
@@ -2,17 +2,12 @@ import React, { Component } from "react";
 import withAuth from "../../components/withAuth";
 import API from "../../utils/API";
 import { Col, Row, Container } from "../../components/Grid";
-<<<<<<< HEAD
 import Form from "../../components/Form/Form";
-=======
-import Form from '../../components/Form/Form';
-import EventCard from '../../components/EventCard/EventCard';
+import EventCard from "../../components/EventCard/EventCard";
 
->>>>>>> f2be6660b840753192017a8cce1f91e5fcfbbf73
 class Dashboard extends Component {
   state = { events: [] };
 
-<<<<<<< HEAD
   componentDidMount() {
     API.getEvents()
       .then(res => {
@@ -33,38 +28,12 @@ class Dashboard extends Component {
             <h1>Dashboard</h1>
           </Col>
         </Row>
+
+        <EventCard />
         <Form />
       </Container>
     );
   }
-=======
-	componentDidMount() {
-		API.getEvents()
-			.then(res => {
-				this.setState({
-					events: res.data
-				});
-				console.log(this.state.events);
-			})
-			.catch(err => {
-				if (err) throw err;
-			});
-	}
-	render() {
-		return (
-			<Container fluid>
-				<Row>
-					<Col size="md-12">
-						<h1>Dashboard</h1>
-					</Col>
-				</Row>
-
-				<EventCard></EventCard>
-				<Form></Form>
-			</Container>
-		);
-	}
->>>>>>> f2be6660b840753192017a8cce1f91e5fcfbbf73
 }
 
 export default withAuth(Dashboard);
