b09a233cabf098311274034f642be71c775f64ac
Manny Barboza <mannyb1077@gmail.com>
2019-07-23 03:10:16 -0600

    tasks-css

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -4,43 +4,40 @@ import { Header, Navigation, Drawer } from "react-mdl";
 require("./style.css");
 
 function Nav() {
-
-    return [
-        <Header key="header" className="header-color" title={<Link style={{ textDecoration: 'none', color: 'white' }} to="/">𝔭𝔩𝔞𝔫𝔡</Link>} scroll>
-            <Navigation>
-                <Link to="/login">
-                    Login
-				</Link>
-                <Link to="/signup">
-                    Signup
-				</Link>
-                <Link to="/about">
-                    About
-				</Link>
-                <Link to="/logout">
-                    Logout
-				</Link>
-            </Navigation>
-        </Header>,
-        <Drawer key="drawer" title={<Link style={{ textDecoration: 'none', color: 'black' }} to="/">
-            𝔭𝔩𝔞𝔫𝔡
-		</Link>}>
-            <Navigation>
-                <Link to="/login">
-                    Login
-				</Link>
-                <Link to="/signup">
-                    Signup
-				</Link>
-                <Link to="/about">
-                    About
-				</Link>
-                <Link to="/logout">
-                    Logout
-				</Link>
-            </Navigation>
-        </Drawer>
-    ];
-};
+  return [
+    <Header
+      key='header'
+      className='header-color'
+      title={
+        <Link style={{ textDecoration: "none", color: "white" }} to='/'>
+          𝔭𝔩𝔞𝔫𝔡
+        </Link>
+      }
+      scroll
+    >
+      <Navigation>
+        <Link to='/login'>Login</Link>
+        <Link to='/signup'>Signup</Link>
+        <Link to='/about'>About</Link>
+        <Link to='/logout'>Logout</Link>
+      </Navigation>
+    </Header>,
+    <Drawer
+      key='drawer'
+      title={
+        <Link style={{ textDecoration: "none", color: "black" }} to='/'>
+          𝔭𝔩𝔞𝔫𝔡
+        </Link>
+      }
+    >
+      <Navigation>
+        <Link to='/login'>Login</Link>
+        <Link to='/signup'>Signup</Link>
+        <Link to='/about'>About</Link>
+        <Link to='/logout'>Logout</Link>
+      </Navigation>
+    </Drawer>
+  ];
+}
 
 export default Nav;
