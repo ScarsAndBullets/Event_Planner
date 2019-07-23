4f5d3f28bf0670e55f23be585668cb79a1872483
Manny Barboza <mannyb1077@gmail.com>
2019-07-23 17:15:14 -0600

    some css

--- client/src/components/Slider/Slider.js
+++ client/src/components/Slider/Slider.js
@@ -1,71 +1,75 @@
 import React, { Component } from "react";
 import {
-	Tabs,
-	Tab,
-	Grid,
-	Cell,
-	Card,
-	CardTitle,
-	CardText,
-	CardActions,
-	Button,
-	CardMenu,
-	IconButton,
-	Layout
+  Tabs,
+  Tab,
+  Grid,
+  Cell,
+  Card,
+  CardTitle,
+  CardText,
+  CardActions,
+  Button,
+  CardMenu,
+  IconButton,
+  Layout
 } from "react-mdl";
 import EventCard from "../../components/EventCard/EventCard";
 import "./Slider.css";
 
 class Slider extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			activeTab: 0
-		};
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeTab: 0
+    };
+  }
 
-	toggleCategories() {
-		if (this.state.activeTab === 0) {
-			return (
-				<Layout>
-					<div className="slider card -grid">
-						{/* <h5>Upcoming Events</h5> */}
-						<Grid>
-							{this.props.events.map(card => (
-								<Cell key={card._id} col={3} tablet={6} phone={12}>
-									<EventCard events={card} />
-								</Cell>
-							))}
-						</Grid>
-					</div>
-				</Layout>
-			);
-		} else if (this.state.activeTab === 1) {
-			return (
-				<div>
-					<p>Coming Soon!</p>
-				</div>
-			);
-		}
-	}
-	render() {
-		return (
-			<Layout>
-				<Tabs
-					activeTab={this.state.activeTab}
-					onChange={tabId => this.setState({ activeTab: tabId })}
-					ripple
-				>
-					<Tab><span className="fontColor">Upcoming Events</span> </Tab>
-					<Tab><span className="fontColor">Past Events</span></Tab>
-				</Tabs>
-				<Grid>
-					<Cell col={12}>
-						<div className="content">{this.toggleCategories()}</div>
-					</Cell>
-				</Grid>
-			</Layout>
-		);
-	}
+  toggleCategories() {
+    if (this.state.activeTab === 0) {
+      return (
+        <Layout>
+          <div className='slider card -grid'>
+            {/* <h5>Upcoming Events</h5> */}
+            <Grid>
+              {this.props.events.map(card => (
+                <Cell key={card._id} col={3} tablet={6} phone={12}>
+                  <EventCard events={card} />
+                </Cell>
+              ))}
+            </Grid>
+          </div>
+        </Layout>
+      );
+    } else if (this.state.activeTab === 1) {
+      return (
+        <div>
+          <p className='past-events'>Coming Soon!</p>
+        </div>
+      );
+    }
+  }
+  render() {
+    return (
+      <Layout>
+        <Tabs
+          activeTab={this.state.activeTab}
+          onChange={tabId => this.setState({ activeTab: tabId })}
+          ripple
+        >
+          <Tab>
+            <span className='fontColor'>Upcoming Events</span>{" "}
+          </Tab>
+          <Tab>
+            <span className='fontColor'>Past Events</span>
+          </Tab>
+        </Tabs>
+        <Grid>
+          <Cell col={12}>
+            <div className='content'>{this.toggleCategories()}</div>
+          </Cell>
+        </Grid>
+      </Layout>
+    );
+  }
 }
 export default Slider;
--- client/src/components/TaskForm/TaskForm.js
+++ client/src/components/TaskForm/TaskForm.js
@@ -6,174 +6,175 @@ import { Col, Row, Container } from "../TasksGrid/TasksGrid";
 import API from "../../utils/API";
 
 class TaskForm extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			taskName: "",
-			participantId: ""
-		};
-		this.handleChange = this.handleChange.bind(this);
-		this.handleSubmit = this.handleSubmit.bind(this);
-		this.getCurrentParticipantId = this.getCurrentParticipantId.bind(this);
-		this.getParticipantInfo = this.getParticipantInfo.bind(this);
-		this.assignTask = this.assignTask.bind(this);
-	}
-	componentDidMount() {
-		setInterval(() => {
-			if (this.state.participantId !== "") {
-				return clearInterval();
-			}
-			this.getCurrentParticipantId();
-		}, 500);
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      taskName: "",
+      participantId: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.getCurrentParticipantId = this.getCurrentParticipantId.bind(this);
+    this.getParticipantInfo = this.getParticipantInfo.bind(this);
+    this.assignTask = this.assignTask.bind(this);
+  }
+  componentDidMount() {
+    setInterval(() => {
+      if (this.state.participantId !== "") {
+        return clearInterval();
+      }
+      this.getCurrentParticipantId();
+    }, 500);
+  }
 
-	getCurrentParticipantId() {
-		if (this.props.userId !== null) {
-			this.props.participants.map(participant => {
-				if (participant.userId === this.props.userId) {
-					return this.setState({ participantId: participant._id });
-				}
-			});
-		}
-	}
-	getParticipantInfo(id) {
-		let participantInfo;
-		this.props.participants.map(participant => {
-			let searchForTask = participant.tasks.map(task => {
-				if (task._id === id) return task;
-			});
-			if (searchForTask.length > 0) participantInfo = participant;
-		});
-		return participantInfo;
-	}
-	handleChange(event) {
-		let name = event.target.name;
-		let value = event.target.value;
-		this.setState({
-			[name]: value
-		});
-	}
+  getCurrentParticipantId() {
+    if (this.props.userId !== null) {
+      this.props.participants.map(participant => {
+        if (participant.userId === this.props.userId) {
+          return this.setState({ participantId: participant._id });
+        }
+      });
+    }
+  }
+  getParticipantInfo(id) {
+    let participantInfo;
+    this.props.participants.map(participant => {
+      let searchForTask = participant.tasks.map(task => {
+        if (task._id === id) return task;
+      });
+      if (searchForTask.length > 0) participantInfo = participant;
+    });
+    return participantInfo;
+  }
+  handleChange(event) {
+    let name = event.target.name;
+    let value = event.target.value;
+    this.setState({
+      [name]: value
+    });
+  }
 
-	handleSubmit(event) {
-		event.preventDefault();
-		if (this.state.taskName === "" || !this.props.userId) return;
-		API.saveTask({
-			taskName: this.state.taskName,
-			eventId: this.props.eventId
-		})
-			.then(res => {
-				this.state.taskName = "";
-				this.props.newTask(res.data);
-			})
-			.catch(err => {
-				console.log(err);
-			});
-	}
-	assignTask(taskId, pId) {
-		API.assignTask({
-			taskId: taskId,
-			participantId: pId
-		})
-			.then(res => {
-				this.props.assignTaskToState(res.data);
-			})
-			.catch(err => {
-				console.log(err);
-			});
-	}
-	unassignTask(taskId, pId) {
-		API.unassignTask({
-			taskId: taskId,
-			participantId: pId
-		})
-			.then(res => {
-				this.props.unassignTaskToState(res.data);
-			})
-			.catch(err => {
-				console.log(err);
-			});
-	}
-	deleteTask(_id) {
-		API.deleteTask(_id).then(res => {
-			console.log("Task Deleted");
-			console.log(_id);
+  handleSubmit(event) {
+    event.preventDefault();
+    if (this.state.taskName === "" || !this.props.userId) return;
+    API.saveTask({
+      taskName: this.state.taskName,
+      eventId: this.props.eventId
+    })
+      .then(res => {
+        this.state.taskName = "";
+        this.props.newTask(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+  assignTask(taskId, pId) {
+    API.assignTask({
+      taskId: taskId,
+      participantId: pId
+    })
+      .then(res => {
+        this.props.assignTaskToState(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+  unassignTask(taskId, pId) {
+    API.unassignTask({
+      taskId: taskId,
+      participantId: pId
+    })
+      .then(res => {
+        this.props.unassignTaskToState(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+  deleteTask(_id) {
+    API.deleteTask(_id).then(res => {
+      console.log("Task Deleted");
+      console.log(_id);
 
-			this.loadTasks();
-		});
-	}
+      this.loadTasks();
+    });
+  }
 
-	render() {
-		return (
-			<div>
-				<h2>Tasks</h2>
-				<form className="NewTaskForm" onSubmit={this.handleSubmit}>
-					<input
-						type="text"
-						placeholder="Add New Task"
-						id="taskName"
-						name="taskName"
-						value={this.state.taskName}
-						onChange={this.handleChange}
-					/>
+  render() {
+    return (
+      <div>
+        <h2>Tasks</h2>
+        <form className='NewTaskForm' onSubmit={this.handleSubmit}>
+          <input
+            type='text'
+            placeholder='Add New Task'
+            id='taskName'
+            name='taskName'
+            value={this.state.taskName}
+            onChange={this.handleChange}
+          />
 
-					<button type="submit">Add Task</button>
-				</form>
-				<ul>
-					{this.props.tasks.map(task => {
-						if (task.taskAssigned) {
-							let pInfo = this.getParticipantInfo(task._id);
+          <button type='submit'>Add Task</button>
+        </form>
+        <ul>
+          {this.props.tasks.map(task => {
+            if (task.taskAssigned) {
+              let pInfo = this.getParticipantInfo(task._id);
 
-							let checkParticipant =
-								this.state.participantId === pInfo._id
-									? () => {
-											this.unassignTask(task._id, this.state.participantId);
-									  }
-									: () => {
-											console.log("not owner");
-									  };
-							return (
-								<li className="Task" onClick={checkParticipant} key={task._id}>
-									<button onClick={this.deleteTask}>
-										<i className="fas fa-trash" />
-									</button>
-									{task.taskName}
+              let checkParticipant =
+                this.state.participantId === pInfo._id
+                  ? () => {
+                      this.unassignTask(task._id, this.state.participantId);
+                    }
+                  : () => {
+                      console.log("not owner");
+                    };
+              return (
+                <li className='Task' onClick={checkParticipant} key={task._id}>
+                  <button onClick={this.deleteTask}>
+                    <i className='fas fa-trash' />
+                  </button>
+                  {task.taskName}
 
-									<div className="Task-buttons">
-										<span className="userId">
-											<h6>{"Assigned To: " + pInfo.name}</h6>
-										</span>
-									</div>
-								</li>
-							);
-						} else {
-							let userCheck = !this.props.userId
-								? () => {
-										console.log("not logged in");
-								  }
-								: () => {
-										this.assignTask(task._id, this.state.participantId);
-								  };
-							return (
-								<li className="Task" onClick={userCheck} key={task._id}>
-									<button onClick={this.deleteTask}>
-										<i className="fas fa-trash" />
-									</button>
-									{task.taskName}
+                  <div className='Task-buttons'>
+                    <span className='userId'>
+                      <h6>{"Assigned To: " + pInfo.name}</h6>
+                    </span>
+                  </div>
+                </li>
+              );
+            } else {
+              let userCheck = !this.props.userId
+                ? () => {
+                    console.log("not logged in");
+                  }
+                : () => {
+                    this.assignTask(task._id, this.state.participantId);
+                  };
+              return (
+                <li className='Task' onClick={userCheck} key={task._id}>
+                  <button onClick={this.deleteTask}>
+                    <i className='fas fa-trash' />
+                  </button>
 
-									<div className="Task-buttons">
-										<h6>Assigned To:</h6>
-										<span className="userId">
-											<h6>Not Assigned</h6>
-										</span>
-									</div>
-								</li>
-							);
-						}
-					})}
-				</ul>
-			</div>
-		);
-	}
+                  {task.taskName}
+
+                  <div className='Task-buttons'>
+                    <h6>Assigned To:</h6>
+                    <span className='userId'>
+                      <h6>Not Assigned</h6>
+                    </span>
+                  </div>
+                </li>
+              );
+            }
+          })}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default TaskForm;
