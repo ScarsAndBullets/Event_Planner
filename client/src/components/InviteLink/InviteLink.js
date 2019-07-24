08e8ffaa027ad437b2828ed26e2a031e2d9114a1
Manny Barboza <mannyb1077@gmail.com>
2019-07-23 18:16:46 -0600

    WTF

--- client/src/components/InviteLink/InviteLink.js
+++ client/src/components/InviteLink/InviteLink.js
@@ -1,69 +1,72 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
-import { makeStyles } from '@material-ui/core/styles';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import IconButton from '@material-ui/core/IconButton';
-import { CheckCircleRounded } from '@material-ui/icons';
-import Slide from '@material-ui/core/Slide';
+import React from "react";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import IconButton from "@material-ui/core/IconButton";
+import { CheckCircleRounded } from "@material-ui/icons";
+import Slide from "@material-ui/core/Slide";
 
 import api from "./../../utils/API";
 
 const Transition = React.forwardRef(function Transition(props, ref) {
-    return <Slide direction="up" ref={ref} {...props} />;
+  return <Slide direction='up' ref={ref} {...props} />;
 });
 
 function InviteLink(props) {
-    const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = React.useState(false);
 
-    function handleClickOpen() {
-        api.sendEmail(props.emailValue, props.eventId);
-        api.addParticipant(props.emailValue, props.eventId);
-        setOpen(true);
-    }
+  function handleClickOpen() {
+    //api.sendEmail(props.emailValue, props.eventId);
+    api.addParticipant(props.emailValue, props.eventId);
+    setOpen(true);
+  }
 
-    function handleClose() {
-        setOpen(false);
-    }
+  function handleClose() {
+    setOpen(false);
+  }
 
-    return (
-        <div>
-            <IconButton
-                // className={iconBttnClass.button}
-                aria-label="Create"
-                onClick={handleClickOpen}
-                color="primary"
-            ><CheckCircleRounded fontSize="large" />
-                {/* <InviteLink /> */}
-            </IconButton>
-            <Dialog
-                open={open}
-                TransitionComponent={Transition}
-                keepMounted
-                onClose={handleClose}
-                aria-labelledby="alert-dialog-slide-title"
-                aria-describedby="alert-dialog-slide-description"
-            >
-                {/* CENTER TITLE */}
-                <DialogTitle id="alert-dialog-slide-title">{"An invitation has been sent!"}</DialogTitle>
-                <DialogContent>
-                    <DialogContentText id="alert-dialog-slide-description">
-                        It's crucial they open this email and click the provided link, or else they won't be able to see the event details. If they don't see the email, make sure they check their junk or spam folders.
-
+  return (
+    <div>
+      <IconButton
+        // className={iconBttnClass.button}
+        aria-label='Create'
+        onClick={handleClickOpen}
+        color='primary'
+      >
+        <CheckCircleRounded fontSize='large' />
+        {/* <InviteLink /> */}
+      </IconButton>
+      <Dialog
+        open={open}
+        TransitionComponent={Transition}
+        keepMounted
+        onClose={handleClose}
+        aria-labelledby='alert-dialog-slide-title'
+        aria-describedby='alert-dialog-slide-description'
+      >
+        {/* CENTER TITLE */}
+        <DialogTitle id='alert-dialog-slide-title'>
+          {"An invitation has been sent!"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id='alert-dialog-slide-description'>
+            It's crucial they open this email and click the provided link, or
+            else they won't be able to see the event details. If they don't see
+            the email, make sure they check their junk or spam folders.
           </DialogContentText>
-                    {/* CENTER and BOLD LINK */}
-
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleClose} color="primary">
-                        Got it!
+          {/* CENTER and BOLD LINK */}
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color='primary'>
+            Got it!
           </Button>
-                </DialogActions>
-            </Dialog>
-        </div>
-    );
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
 }
 export default InviteLink;
--- client/src/pages/EventView/index.js
+++ client/src/pages/EventView/index.js
@@ -19,140 +19,147 @@ import TaskForm from "../../components/TaskForm/TaskForm";
 import API from "../../utils/API";
 import EventInfo from "../../components/EventInfo";
 import Participants from "./../../components/Participants";
-
+import { withRouter } from "react-router-dom";
 class EventView extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			title: "",
-			details: "",
-			date: "",
-			time: "",
-			location: "",
-			requirements: "",
-			userId: "",
-			eventOwnerId: "",
-			tasks: [],
-			participants: [],
-			eventId: ""
-		};
-		this.updateParticipantGoing = this.updateParticipantGoing.bind(this);
-		this.newTask = this.newTask.bind(this);
-		this.assignTaskToState = this.assignTaskToState.bind(this);
-		this.unassignTaskToState = this.unassignTaskToState.bind(this);
-	}
-	componentDidMount() {
-		const {
-			match: { params }
-		} = this.props;
-		API.getEventView(params.id).then(event => {
-			event = event.data;
-			this.setState({
-				title: event.title,
-				details: event.details,
-				date: event.date,
-				time: event.time,
-				location: event.location,
-				requirements: event.requirements,
-				tasks: event.tasks,
-				participants: event.participants,
-				userId: event.userId,
-				eventOwnerId: event.eventOwnerId,
-				eventId: event.eventId
-			});
-			console.log(this.state);
-		});
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "",
+      details: "",
+      date: "",
+      time: "",
+      location: "",
+      requirements: "",
+      userId: "",
+      eventOwnerId: "",
+      tasks: [],
+      participants: [],
+      eventId: ""
+    };
+    this.updateParticipantGoing = this.updateParticipantGoing.bind(this);
+    this.newTask = this.newTask.bind(this);
+    this.assignTaskToState = this.assignTaskToState.bind(this);
+    this.unassignTaskToState = this.unassignTaskToState.bind(this);
+  }
+  componentDidMount() {
+    const {
+      match: { params }
+    } = this.props;
+    API.getEventView(params.id).then(event => {
+      event = event.data;
+      this.setState({
+        title: event.title,
+        details: event.details,
+        date: event.date,
+        time: event.time,
+        location: event.location,
+        requirements: event.requirements,
+        tasks: event.tasks,
+        participants: event.participants,
+        userId: event.userId,
+        eventOwnerId: event.eventOwnerId,
+        eventId: event.eventId
+      });
+      console.log(this.state);
+    });
+  }
 
-	//Find participant in state by id, switch attending to true if false, or vice versa.
-	updateParticipantGoing(id) {
-		this.state.participants.map(participant => {
-			if (participant._id === id) {
-				participant.attending
-					? (participant.attending = false)
-					: (participant.attending = true);
-				return this.setState({
-					participants: this.state.participants
-				});
-			}
-		});
-	}
+  //Find participant in state by id, switch attending to true if false, or vice versa.
+  updateParticipantGoing(id) {
+    this.state.participants.map(participant => {
+      if (participant._id === id) {
+        participant.attending
+          ? (participant.attending = false)
+          : (participant.attending = true);
+        return this.setState({
+          participants: this.state.participants
+        });
+      }
+    });
+  }
 
-	newTask(task) {
-		this.state.tasks.push(task);
-		this.setState({
-			tasks: this.state.tasks
-		});
-	}
+  newTask(task) {
+    this.state.tasks.push(task);
+    this.setState({
+      tasks: this.state.tasks
+    });
+  }
 
-	assignTaskToState(taskAndParticipant) {
-		let participant = taskAndParticipant.participant;
-		let task = taskAndParticipant.task;
-		this.state.tasks.map(stateTask => {
-			if (task._id === stateTask._id) {
-				stateTask.taskAssigned = true;
-				stateTask.strikeThrough = true;
-			}
-		});
-		this.state.participants.map(stateParticipant => {
-			if (participant._id === stateParticipant._id) {
-				stateParticipant.tasks.push(task);
-			}
-		});
-		this.setState({
-			tasks: this.state.tasks,
-			participants: this.state.participants
-		});
-	}
+  assignTaskToState(taskAndParticipant) {
+    let participant = taskAndParticipant.participant;
+    let task = taskAndParticipant.task;
+    this.state.tasks.map(stateTask => {
+      if (task._id === stateTask._id) {
+        stateTask.taskAssigned = true;
+        stateTask.strikeThrough = true;
+      }
+    });
+    this.state.participants.map(stateParticipant => {
+      if (participant._id === stateParticipant._id) {
+        stateParticipant.tasks.push(task);
+      }
+    });
+    this.setState({
+      tasks: this.state.tasks,
+      participants: this.state.participants
+    });
+  }
 
-	unassignTaskToState(taskAndParticipant) {
-		console.log(this.state);
-		let participant = taskAndParticipant.participant;
-		let task = taskAndParticipant.task;
-		this.state.tasks.map(stateTask => {
-			if (task._id === stateTask._id) {
-				stateTask.taskAssigned = false;
-				stateTask.strikeThrough = false;
-			}
-		});
-		this.state.participants.filter(stateParticipant => {
-			if (participant._id !== stateParticipant._id) {
-				return stateParticipant;
-			}
-		});
-		this.setState({
-			tasks: this.state.tasks,
-			participants: this.state.participants
-		});
-	}
+  unassignTaskToState(taskAndParticipant) {
+    console.log(this.state);
+    let participant = taskAndParticipant.participant;
+    let task = taskAndParticipant.task;
+    this.state.tasks.map(stateTask => {
+      if (task._id === stateTask._id) {
+        stateTask.taskAssigned = false;
+        stateTask.strikeThrough = false;
+      }
+    });
+    this.state.participants.filter(stateParticipant => {
+      if (participant._id !== stateParticipant._id) {
+        return stateParticipant;
+      }
+    });
+    this.setState({
+      tasks: this.state.tasks,
+      participants: this.state.participants
+    });
+  }
 
-	deleteTask(taskId) { }
+  deleteTask(taskId) {}
 
-	render() {
-		return (
-			<div>
-				<AddParticipant />
-				<EventInfo
-					title={this.state.title}
-					details={this.state.details}
-					date={this.state.date}
-					time={this.state.time}
-					location={this.state.location}
-					requirements={this.state.requirements}
-				/>
-				<Participants participants={this.state.participants} />
-				<TaskForm
-					tasks={this.state.tasks}
-					participants={this.state.participants}
-					newTask={this.newTask}
-					assignTaskToState={this.assignTaskToState}
-					unassignTaskToState={this.unassignTaskToState}
-					eventId={this.state.eventId}
-					userId={this.state.userId}
-				/>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <button
+          onClick={() => {
+            this.props.history.push("/dashboard");
+          }}
+        >
+          Back to Dashboard!
+        </button>
+        {/* <AddParticipant /> */}
+        <EventInfo
+          title={this.state.title}
+          details={this.state.details}
+          date={this.state.date}
+          time={this.state.time}
+          location={this.state.location}
+          requirements={this.state.requirements}
+        />
+        <Participants participants={this.state.participants} />
+        <TaskForm
+          tasks={this.state.tasks}
+          participants={this.state.participants}
+          newTask={this.newTask}
+          assignTaskToState={this.assignTaskToState}
+          unassignTaskToState={this.unassignTaskToState}
+          eventId={this.state.eventId}
+          userId={this.state.userId}
+        />
+      </div>
+    );
+  }
 }
 
-export default EventView;
+export default withRouter(EventView);
